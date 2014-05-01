175cf6876bff49dc02dcfd38828f323106665483
Carmen Alvarez <c@rmen.ca>
2014-05-01 15:10:14 +0200

    Changed source files encoding to utf-8

--- src/ca/rmen/android/palidamuerte/app/category/list/CategoriesCursorAdapter.java
+++ src/ca/rmen/android/palidamuerte/app/category/list/CategoriesCursorAdapter.java
@@ -1,20 +1,20 @@
 /**
  * Copyright 2014 Carmen Alvarez
  *
- * This file is part of P‡lida Muerte.
+ * This file is part of PÃ¡lida Muerte.
  *
- * P‡lida Muerte is free software: you can redistribute it and/or modify
+ * PÃ¡lida Muerte is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * P‡lida Muerte is distributed in the hope that it will be useful,
+ * PÃ¡lida Muerte is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with P‡lida Muerte. If not, see <http://www.gnu.org/licenses/>.
+ * along with PÃ¡lida Muerte. If not, see <http://www.gnu.org/licenses/>.
  */
 package ca.rmen.android.palidamuerte.app.category.list;
 
--- src/ca/rmen/android/palidamuerte/app/poem/detail/PoemDetailActivity.java
+++ src/ca/rmen/android/palidamuerte/app/poem/detail/PoemDetailActivity.java
@@ -1,20 +1,20 @@
 /**
  * Copyright 2014 Carmen Alvarez
  *
- * This file is part of P�lida Muerte.
+ * This file is part of Pálida Muerte.
  *
- * P�lida Muerte is free software: you can redistribute it and/or modify
+ * Pálida Muerte is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * P�lida Muerte is distributed in the hope that it will be useful,
+ * Pálida Muerte is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with P�lida Muerte. If not, see <http://www.gnu.org/licenses/>.
+ * along with Pálida Muerte. If not, see <http://www.gnu.org/licenses/>.
  */
 package ca.rmen.android.palidamuerte.app.poem.detail;
 
