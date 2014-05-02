3d92f5dcf1c783c28b7183cbb00c0fd5aaa71062
Carmen Alvarez <c@rmen.ca>
2014-05-02 23:38:20 +0200

    Added app version name to about screen

--- src/ca/rmen/android/palidamuerte/app/about/AboutActivity.java
+++ src/ca/rmen/android/palidamuerte/app/about/AboutActivity.java
@@ -20,18 +20,23 @@ package ca.rmen.android.palidamuerte.app.about;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import ca.rmen.android.palidamuerte.Constants;
 import ca.rmen.android.palidamuerte.R;
 import ca.rmen.android.palidamuerte.ui.ActionBar;
 import ca.rmen.android.palidamuerte.ui.Font;
 
 public class AboutActivity extends Activity { // NO_UCD (use default)
+    private static final String TAG = Constants.TAG + AboutActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +70,14 @@ public class AboutActivity extends Activity { // NO_UCD (use default)
             TextView tvTitleView = (TextView) rootView.findViewById(R.id.author_name);
             Typeface font = Font.getTypeface(getActivity());
             tvTitleView.setTypeface(font);
+            PackageInfo pInfo;
+            try {
+                pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+                TextView tvAppNameAndVersion = (TextView) rootView.findViewById(R.id.app_name_and_version);
+                tvAppNameAndVersion.setText(getString(R.string.app_name_and_version, getString(R.string.app_name), pInfo.versionName));
+            } catch (NameNotFoundException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
             return rootView;
         }
     }
