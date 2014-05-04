1773fdba2d2bf530f9f14c81f78aec1c008bbf0c
Carmen Alvarez <c@rmen.ca>
2014-05-04 20:11:48 +0200

    Fixed encoding of a couple of source files

--- src/ca/rmen/android/palidamuerte/app/poem/list/Search.java
+++ src/ca/rmen/android/palidamuerte/app/poem/list/Search.java
@@ -1,20 +1,20 @@
 /**
  * Copyright 2014 Carmen Alvarez
  *
- * This file is part of Pálida Muerte.
+ * This file is part of P√°lida Muerte.
  *
- * Pálida Muerte is free software: you can redistribute it and/or modify
+ * P√°lida Muerte is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * Pálida Muerte is distributed in the hope that it will be useful,
+ * P√°lida Muerte is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Pálida Muerte. If not, see <http://www.gnu.org/licenses/>.
+ * along with P√°lida Muerte. If not, see <http://www.gnu.org/licenses/>.
  */
 package ca.rmen.android.palidamuerte.app.poem.list;
 
@@ -25,7 +25,7 @@ import ca.rmen.android.palidamuerte.provider.poem.PoemSelection;
 
 public class Search {
 
-    private static final String ACCENTS = "áéíóúñÁÉÍÓÚÑ";
+    private static final String ACCENTS = "√°√©√≠√≥√∫√±√Å√â√ç√ì√ö√ë";
     private static final String NO_ACCENTS = "aeiounaeioun";
     private static final int SEARCH_CONTEXT_SIZE = 100;
     private static final String[] SEARCH_COLUMNS = new String[] { PoemColumns.YEAR, PoemColumns.LOCATION, PoemColumns.TITLE, PoemColumns.PRE_CONTENT,
